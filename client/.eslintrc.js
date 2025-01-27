f0c8a8932598eac864f698c401c33b39842a90ff
Creatman <creatmanick@gmail.com>
2025-01-27 11:40:44 +0300

    Add ESLint configuration for React client

---
+++ client/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2024: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2024,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  plugins: [
+    'react',
+    'react-hooks'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off'
+  }
+}
